c4d5680b5c4cb60761077ae4d4a38f09c9329447
Waiwaitiyyt <endesuka_@outlook.com>
2026-02-28 22:57:17 +0800

    modified:   CMakeLists.txt 	modified:   run.py

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 """
-程序启动脚本
+Fun script
 """
 import sys
 from pathlib import Path
 
-# 添加src/python到路径
 project_root = Path(__file__).parent
 sys.path.insert(0, str(project_root / "src" / "python"))
-
-# 导入并运行主程序
 from src.python.main import main
 
 if __name__ == "__main__":
